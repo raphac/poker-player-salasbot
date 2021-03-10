b438acb053373fac43f8cab6a6315358d66ef1b0
cnworks <christianoetzli@gmail.com>
2021-03-10 14:48:56 +0100

    yolo pre flop

--- src/CardAnalyzerService.cs
+++ src/CardAnalyzerService.cs
@@ -44,12 +44,7 @@ namespace Nancy.Simple
         {
             var firstRank = RankValueByRank[first.Rank];
             var secondRank = RankValueByRank[second.Rank];
-            if (firstRank >= 8 && secondRank >= 8)
-            {
-                return firstRank + secondRank;
-            }
-
-            return 0;
+            return firstRank + secondRank;
         }
 
         private int PairValue(Card[] handCards, Card[] communityCards)
