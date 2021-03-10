ae06350a8aa480c4cc65638a2ea2bdd609497893
cnworks <christianoetzli@gmail.com>
2021-03-10 11:30:13 +0100

    implement hands

--- src/CardAnalyzerService.cs
+++ src/CardAnalyzerService.cs
@@ -11,13 +11,83 @@ namespace Nancy.Simple
             var ownFirstCard = handCards.First();
             var ownSecondCard = handCards.Last();
 			
-            var shouldBet = RankValueByRank[ownFirstCard.Rank] == RankValueByRank[ownSecondCard.Rank] ||
-                            handCards.All(card => RankValueByRank[card.Rank] >= 10) ||
-                            !((RankValueByRank[ownFirstCard.Rank] == 2 && RankValueByRank[ownFirstCard.Rank] == 7) ||
-                              (RankValueByRank[ownFirstCard.Rank] == 7 && RankValueByRank[ownFirstCard.Rank] == 2));
-            
+            var shouldBet = IsPair(handCards, communityCards) ||
+                            IsTwoPair(handCards, communityCards) ||
+                            IsThreeOfKind(handCards, communityCards) ||
+                            IsFourOfKind(handCards, communityCards) ||
+                            IsFlush(handCards, communityCards) ||
+                            IsFullHouse(handCards, communityCards) ||
+                            handCards.All(card => RankValueByRank[card.Rank] >= 10) &&
+                            !IsBadHand(ownFirstCard);
+
             return shouldBet;
         }
+
+        private static bool IsBadHand(Card ownFirstCard)
+        {
+            return (RankValueByRank[ownFirstCard.Rank] == 2 && RankValueByRank[ownFirstCard.Rank] == 7) ||
+                     (RankValueByRank[ownFirstCard.Rank] == 7 && RankValueByRank[ownFirstCard.Rank] == 2);
+        }
+
+        private bool IsPair(Card[] handCards, Card[] communityCards)
+        {
+            var duplicates = handCards.Union(communityCards).GroupBy(card => RankValueByRank[card.Rank])
+                .Where(g => g.Count() == 2)
+                .Select(y => y.Key)
+                .ToList();
+            return duplicates.Count == 1;
+        }
+        
+        private bool IsTwoPair(Card[] handCards, Card[] communityCards)
+        {
+            var duplicates = handCards.Union(communityCards).GroupBy(card => RankValueByRank[card.Rank])
+                .Where(g => g.Count() == 2)
+                .Select(y => y.Key)
+                .ToList();
+            return duplicates.Count == 2;
+        }
+        
+        private bool IsThreeOfKind(Card[] handCards, Card[] communityCards)
+        {
+            var duplicates = handCards.Union(communityCards).GroupBy(card => RankValueByRank[card.Rank])
+                .Where(g => g.Count() == 3)
+                .Select(y => y.Key)
+                .ToList();
+            return duplicates.Count > 0;
+        }
+        
+        private bool IsFourOfKind(Card[] handCards, Card[] communityCards)
+        {
+            var duplicates = handCards.Union(communityCards).GroupBy(card => RankValueByRank[card.Rank])
+                .Where(g => g.Count() == 4)
+                .Select(y => y.Key)
+                .ToList();
+            return duplicates.Count > 0;
+        }
+        
+        private bool IsFullHouse(Card[] handCards, Card[] communityCards)
+        {
+            return IsThreeOfKind(handCards, communityCards) && IsPair(handCards, communityCards);
+        }
+
+        // private bool IsStraight(Card[] handCards, Card[] communityCards)
+        // {
+        //     
+        // }
+        
+        private bool IsFlush(Card[] handCards, Card[] communityCards)
+        {
+            var duplicates = handCards.Union(communityCards).GroupBy(card => card.Suit)
+                .Where(g => g.Count() >= 5)
+                .Select(y => y.Key)
+                .ToList();
+            return duplicates.Count > 0;
+        }
+        
+        // private bool IsStraightFlush(Card[] handCards, Card[] communityCards)
+        // {
+        //     return IsFlush(handCards, communityCards) && IsStraight(handCards, communityCards);
+        // }
 		
         private static IDictionary<string, int> RankValueByRank = new Dictionary<string, int>
         {
